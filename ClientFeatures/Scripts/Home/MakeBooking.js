669e673be1ebdae14f9e6f0029de9f6c30a4c40b
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-12 12:36:53 +0400

    Создаем по соглашению папку для контроллера и переносим скирпт в отдельный файл

---
+++ ClientFeatures/Scripts/Home/MakeBooking.js
@@ -0,0 +1,14 @@
+﻿function processResponse(appt) {
+    $('#successClientName').text(appt.ClientName);
+    switchViews();
+}
+
+function switchViews() {
+    $(".hidden, .visible").toggleClass("hidden visible");
+}
+
+$(document).ready(function () {
+    $('#backButton').click(function (e) {
+        switchViews();
+    });
+});
