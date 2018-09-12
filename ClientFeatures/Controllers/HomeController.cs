617c99dd5273fcd1c5ab20a3cc735f359c356752
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-12 11:41:44 +0400

    Подготовка проекта для главы

---
+++ ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ClientFeatures.Models;
+using System;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Adam",
+                TermsAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            // инструкции для хранения нового Appointment
+            // будут идти здесь в реальном проекте
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
